c065336c1d32fe344fdb91235ae2e7efa53c9714
FedericoCortesi <cortesifederico6@gmail.com>
2025-12-20 12:16:55 -0500

    Add benchmark results and Slurm job script

---
+++ src/benchmarks/results.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+from datetime import datetime 
+import os
+
+import pandas as pd
+
+from utils.paths import VOL_EXPERIMENTS_DIR, BENCHMARKS_DIR
+
+
+def main():
+    all_dfs = []
+    # iterate over directory
+    for file in os.listdir(VOL_EXPERIMENTS_DIR):
+        # restriction
+        if 'ensemble' not in file:
+            continue
+
+        file_path = Path(VOL_EXPERIMENTS_DIR) / file
+        trials = os.listdir(file_path)
+        if 'trial_search_best' in trials:
+            trial ='trial_search_best'
+        else:
+            try:
+                trial = trials[-1]
+            except:
+                print(f'No trials found for {file}')
+
+        # define path
+        analysis_path = f'{file_path}/{trial}/analysis/fold_avg_metrics.csv'
+        
+
+        # read and append
+        try:
+            df = pd.read_csv(analysis_path)
+            
+            # Replace NN with name
+            df['Model'] = df['Model'].replace({'NN': file})
+
+            all_dfs.append(df)
+        except:
+            print(f'Cannot find: {analysis_path}')
+
+    # concat all
+    df_out = pd.concat(all_dfs, axis=0)
+    
+    # delete duplicates
+    df_out = df_out.drop_duplicates()
+
+    print('Shape: ', df_out.shape)
+
+    time = datetime.now() 
+    name = f'results_ensemble_{time:%d%m%Y_%H%M%S}.csv'
+    out_path = BENCHMARKS_DIR / 'tables' /name 
+    
+    df_out.to_csv(out_path, index=False)
+    print('Saved to: ', out_path.resolve())
+
+    return 
+
+if __name__ == '__main__':
+    main()
+
